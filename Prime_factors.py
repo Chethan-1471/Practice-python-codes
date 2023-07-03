c1c773340922c4cca11c30e4474c0aef9e0058f1
Chethan <103055240+Chethan-1471@users.noreply.github.com>
2023-07-03 23:37:31 +0530

    Add files via upload

---
+++ Prime_factors.py
@@ -0,0 +1,18 @@
+#Prime factorization is a mathematical process of breaking down a number into its prime factors.
+
+
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+
+num = int(input("Please enter a number: "))
+print("Prime factors of", num, "are:", prime_factors(num))
